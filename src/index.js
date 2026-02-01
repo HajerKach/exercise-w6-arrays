6d613bf64bac3a7bcaf47339d6352f2642d561dc
Hajer K <hajerk@MacBookAir.lan>
2026-02-01 18:48:07 +0100

    finished the excerise

--- src/index.js
+++ src/index.js
@@ -65,26 +65,58 @@ const notTheCs = () => {
 };
 notTheCs();
 
+// print out name and index of all pokemons
+// like: number x - Squirtle
+
 const nameAndIdThanks = () => {
-  // print out name and index of all pokemons
-  // like: number x - Squirtle
+  pokemons.forEach((item, index) => {
+    console.log(`Number ${index + 1} is on the followint pokemon ${item}`);
+  })
+
 };
+nameAndIdThanks();
+
+// add a pokemon with a name of your choice to the list,
+// print the list so you see its there.
 
 const catchPokemon = name => {
-  // add a pokemon with a name of your choice to the list,
-  // print the list so you see its there.
+  pokemons.push(name)
+  console.log(pokemons);
+
 };
+catchPokemon("Bambu");
+
+// check the pokemons to see if a specific pokemon is in the array
 
 const didICatchIt = name => {
-  // check the pokemons to see if a specific pokemon is in the array
+  console.log(pokemons.includes(name));
+
 };
+didICatchIt("Weedle");
+
+// add the pokemon "Clefairy" in the third place of the array
+// print the list so you see its there.
 
 const addInThirdPlace = () => {
-  // add the pokemon "Clefairy" in the third place of the array
-  // print the list so you see its there.
+  pokemons.splice(2, 0, "Clefairy",);
+  console.log(pokemons);
 };
+addInThirdPlace();
 
 // ***BONUS***
+// // find the pokemon with the longest name
+
 const theLongestName = () => {
-  // find the pokemon with the longest name
+  let longstName = [];
+  let maxLength = 0;
+  for (let pokemon of pokemons) {
+    if (pokemon.length > maxLength) {
+      maxLength = pokemon.length;
+      longstName = [pokemon];
+    } else if (pokemon.length === maxLength) {
+      longstName.push(pokemon);
+    }
+  }
+  console.log(`De med längst namn finns i följande lista:`, longstName);
 };
+theLongestName();
